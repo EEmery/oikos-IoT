502dc2dddabc72393b0b0bd156fb766d79ddc92f
Eduardo Emery <emeryecs@gmail.com>
2017-02-22 09:50:42 -0300

    Add database uniformization function

--- arduino/python-reader.py
+++ arduino/python-reader.py
@@ -12,12 +12,47 @@ config = {
   	"serviceAccount": "./oikos-gateways-key.json"
 }
 firebase = pyrebase.initialize_app(config)
-database = firebase.database().get().val()
+database = firebase.database()
 
-def print_database(database=database):
-	for house in database:
+def print_database(snapshot=database.get().val()):
+	"""
+	Prints the database formated. Database should be two children depth (houses:necessities).
+
+	@snapshot: a "firebase.database().get().val()" value.
+
+	@return: none.
+	"""
+	for house in snapshot:
 		print "\n" + house
-		for necessity in database[house]:
-			print "    " + necessity + ": " + database[house][necessity]
+		for necessity in snapshot[house]:
+			print "    " + necessity + ": " + snapshot[house][necessity]
 
+
+def unifomize_database(database=database):
+	"""
+	Reads the database template and changes houses keys in order to give them the same necessities.
+	Key values are mainteined. New keys are created with the same value from the template.
+
+	@database: a "firebase.database()" value.
+
+	@returns: none.
+	"""
+	snapshot = database.get().val()
+	template = database.child("template").get().val()
+	template_keys = template.keys()
+	
+	for house in snapshot:
+
+		# Removes old necessities
+		for necessity in snapshot[house]:
+			if not necessity in template_keys:
+				database.child(house).child(necessity).remove()
+
+		# Adds new necessities
+		for necessity in template_keys:
+			if not necessity in snapshot[house]:
+				database.child(house).update({necessity: template[necessity]})
+
+
+unifomize_database()
 print_database()
